984f7d2f0bdcb4785a37076ca641dd84b7445289
Nate Festinger <nate.festinger@gmail.com>
2017-12-22 09:48:11 -0800

    Remove debugging from entry.js

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import { configureStore } from './store/store';
-import PlaylistsReducer from './reducers/entities/playlists-reducer';
-// !!!testing
-import { fetchUsersIndexItems } from './actions/user-actions';
-import { selectUsersIndexItems } from './selectors/selectors';
-import { createFollow, destroyFollow } from './actions/follow-actions';
-import APIUtil from './util/api-util';
-import { addTrackToPlaylist, removeTrackFromPlaylist } from './actions/playlist-actions';
-// !!!
 
 document.addEventListener('DOMContentLoaded', () => {
   let preloadedState;
+  
   if (window.prefetchedData) {
     const { currentUser, playlists, followedUsers } = window.prefetchedData;
+
     preloadedState = {
       session: { currentUser },
       entities: {
@@ -38,13 +32,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const store = configureStore(preloadedState);
-
-  // !!!testing
-  window.store = store;
-  window.APIUtil = APIUtil;
-  window.fetchUsersIndexItems = fetchUsersIndexItems;
-  window.selectUsersIndexItems = selectUsersIndexItems;
-  // !!!
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
